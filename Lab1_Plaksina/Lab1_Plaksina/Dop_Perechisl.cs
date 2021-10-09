c306e387a9e726204119af4390bb0470acb2f9c5
Александра Плаксина <plaksinaaleksandra943@gmail.com>
2021-10-09 09:35:00 +0300

    lab1

---
+++ Lab1_Plaksina/Lab1_Plaksina/Dop_Perechisl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Plaksina
+{
+	public enum Dop_Perechisl
+	{
+		Ten = 10,
+		Twenty = 20,
+		Thrity = 30
+	}
+}
