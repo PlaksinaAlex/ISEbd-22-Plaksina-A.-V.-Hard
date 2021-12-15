ddf815d2f784ec460aa499db0b9bbe9cf628efb6
Александра Плаксина <plaksinaaleksandra943@gmail.com>
2021-12-15 21:48:07 +0400

    lab7

--- Lab1_Plaksina/Lab1_Plaksina/FormAerodrom.cs
+++ Lab1_Plaksina/Lab1_Plaksina/FormAerodrom.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
 
 namespace Lab1_Plaksina
 {
@@ -16,11 +17,14 @@ namespace Lab1_Plaksina
 
 		private LinkedList<Vehicle> link_list;
 
+		private readonly Logger logger;
+
 		public FormAerodrom()
 		{
 			InitializeComponent();
 			aerodromCollection = new AerodromCollection(pictureBoxAerodrom.Width, pictureBoxAerodrom.Height);
 			link_list = new LinkedList<Vehicle>();
+			logger = LogManager.GetCurrentClassLogger();
 			Draw();
 		}
 
@@ -61,19 +65,39 @@ namespace Lab1_Plaksina
 		{
 			if (listBoxAerodrom.SelectedIndex > -1 && maskedTextBox.Text != "")
 			{
-				var airplane = aerodromCollection[listBoxAerodrom.SelectedItem.ToString()] - Convert.ToInt32(maskedTextBox.Text);
-				link_list.AddLast(airplane);
-				Draw();
+				try
+				{
+					var aer = aerodromCollection[listBoxAerodrom.SelectedItem.ToString()] - Convert.ToInt32(maskedTextBox.Text);
+					if (aer != null)
+					{
+						FormAerobus form = new FormAerobus();
+						form.SetAir(aer);
+						form.ShowDialog();
+						logger.Info($"Изъят самолет {aer} с места {maskedTextBox.Text}");
+						Draw();
+					}
+				}
+				catch (AerodromNotFoundException ex)
+				{
+					MessageBox.Show(ex.Message, "Не найдено", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					logger.Error("Не найден самолет");
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message, "Неизвестная ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					logger.Fatal("Вызвана неизвестная ошибка");
+				}
 			}
 		}
 		private void buttonAddAerodrom_Click(object sender, EventArgs e)
 		{
 			if (string.IsNullOrEmpty(textBoxNewLevelName.Text))
 			{
-				MessageBox.Show("Введите название аэродрома", "Ошибка",
-			   MessageBoxButtons.OK, MessageBoxIcon.Error);
+				MessageBox.Show("Введите название аэродрома", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				logger.Warn("Было введено пустное название аэродрома");
 				return;
 			}
+			logger.Info($"Добавили аэродром {textBoxNewLevelName.Text}");
 			aerodromCollection.AddAerodrom(textBoxNewLevelName.Text);
 			ReloadLevels();
 		}
@@ -85,6 +109,7 @@ namespace Lab1_Plaksina
 				if (MessageBox.Show($"Удалить аэродром { listBoxAerodrom.SelectedItem.ToString()}?", "Удаление", MessageBoxButtons.YesNo,
 MessageBoxIcon.Question) == DialogResult.Yes)
 				{
+					logger.Info($"Удалили аэродром {listBoxAerodrom.SelectedItem.ToString()}");
 					aerodromCollection.DelAerodrom(textBoxNewLevelName.Text);
 					ReloadLevels();
 				}
@@ -92,6 +117,7 @@ MessageBoxIcon.Question) == DialogResult.Yes)
 		}
 		private void listBoxAerodrom_SelectedIndexChanged(object sender, EventArgs e)
 		{
+			logger.Info($"Перешли на аэродром {listBoxAerodrom.SelectedItem.ToString()}");
 			Draw();
 		}
 
@@ -106,13 +132,30 @@ MessageBoxIcon.Question) == DialogResult.Yes)
 		{
 			if (aer != null && listBoxAerodrom.SelectedIndex > -1)
 			{
-				if ((aerodromCollection[listBoxAerodrom.SelectedItem.ToString()]) + aer)
+				try
 				{
+					if ((aerodromCollection[listBoxAerodrom.SelectedItem.ToString()]) + aer)
+					{
+						Draw();
+						logger.Info($"Добавлен самолет {aer}");
+					}
+					else
+					{
+						logger.Error("Самолет не удалось поставить");
+						MessageBox.Show("Самолет не удалось поставить");
+					}
+
 					Draw();
 				}
-				else
+				catch (AerodromOverflowException ex)
 				{
-					MessageBox.Show("Самолет не удалось поставить");
+					MessageBox.Show(ex.Message, "Переполнение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					logger.Warn("Аэродром переполнен");
+				}
+				catch (Exception ex)
+				{
+					logger.Fatal("Вызвана неизвестная ошибка");
+					MessageBox.Show(ex.Message, "Неизвестная ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				}
 			}
 		}
@@ -136,15 +179,16 @@ MessageBoxIcon.Question) == DialogResult.Yes)
 		{
 			if (saveFileDialog.ShowDialog() == DialogResult.OK)
 			{
-				if (aerodromCollection.SaveData(saveFileDialog.FileName))
+				try
 				{
-					MessageBox.Show("Сохранение прошло успешно", "Результат",
-				   MessageBoxButtons.OK, MessageBoxIcon.Information);
+					aerodromCollection.SaveData(saveFileDialog.FileName);
+					MessageBox.Show("Сохранение прошло успешно", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					logger.Info("Сохранено в файл " + saveFileDialog.FileName);
 				}
-				else
+				catch (Exception ex)
 				{
-					MessageBox.Show("Не сохранилось", "Результат",
-				   MessageBoxButtons.OK, MessageBoxIcon.Error);
+					logger.Fatal("Вызвана неизвестная ошибка при сохранении");
+					MessageBox.Show(ex.Message, "Неизвестная ошибка при сохранении", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				}
 			}
 		}
@@ -153,17 +197,23 @@ MessageBoxIcon.Question) == DialogResult.Yes)
 		{
 			if (openFileDialog.ShowDialog() == DialogResult.OK)
 			{
-				if (aerodromCollection.LoadAerodromCollection(openFileDialog.FileName))
+				try
 				{
-					MessageBox.Show("Загрузили", "Результат", MessageBoxButtons.OK,
-				   MessageBoxIcon.Information);
+					aerodromCollection.LoadAerodromCollection(openFileDialog.FileName);
+					MessageBox.Show("Загрузили", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					logger.Info("Загружено из файла " + openFileDialog.FileName);
 					ReloadLevels();
 					Draw();
 				}
-				else
+				catch (AerodromOccupiedPlaceException ex)
 				{
-					MessageBox.Show("Не загрузили", "Результат", MessageBoxButtons.OK,
-				   MessageBoxIcon.Error);
+					MessageBox.Show(ex.Message, "Занятое место", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					logger.Warn("Не найден самолет с заданным индексом");
+				}
+				catch (Exception ex)
+				{
+					logger.Fatal("Вызвана неизвестная ошибка при загрузки");
+					MessageBox.Show(ex.Message, "Неизвестная ошибка при загрузки", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				}
 			}
 		}
@@ -172,18 +222,16 @@ MessageBoxIcon.Question) == DialogResult.Yes)
 		{
 			if (saveFileDialog.ShowDialog() == DialogResult.OK)
 			{
-				if (listBoxAerodrom.SelectedIndex > -1)
+				try
 				{
-					if (aerodromCollection.SaveData(saveFileDialog.FileName, listBoxAerodrom.SelectedItem.ToString()))
-					{
-						MessageBox.Show("Сохранение прошло успешно", "Результат",
-						MessageBoxButtons.OK, MessageBoxIcon.Information);
-					}
-					else
-					{
-						MessageBox.Show("Не сохранилось", "Результат",
-						MessageBoxButtons.OK, MessageBoxIcon.Error);
-					}
+					aerodromCollection.SaveData(saveFileDialog.FileName, listBoxAerodrom.SelectedItem.ToString());
+					MessageBox.Show("Сохранение прошло успешно", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					logger.Info("Сохранено в файл " + saveFileDialog.FileName);
+				}
+				catch (Exception ex)
+				{
+					logger.Fatal("Вызвана неизвестная ошибка при сохранении");
+					MessageBox.Show(ex.Message, "Неизвестная ошибка при сохранении", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				}
 			}
 		}
@@ -192,17 +240,23 @@ MessageBoxIcon.Question) == DialogResult.Yes)
 		{
 			if (openFileDialog.ShowDialog() == DialogResult.OK)
 			{
-				if (aerodromCollection.LoadAerodrom(openFileDialog.FileName))
+				try
 				{
-					MessageBox.Show("Загрузили", "Результат", MessageBoxButtons.OK,
-					MessageBoxIcon.Information);
+					aerodromCollection.LoadAerodrom(openFileDialog.FileName);
+					MessageBox.Show("Загрузили", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					logger.Info("Загружено из файла " + openFileDialog.FileName);
 					ReloadLevels();
 					Draw();
 				}
-				else
+				catch (AerodromOccupiedPlaceException ex)
 				{
-					MessageBox.Show("Не загрузили", "Результат", MessageBoxButtons.OK,
-					MessageBoxIcon.Error);
+					MessageBox.Show(ex.Message, "Занятое место", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					logger.Warn("Не найден самолет с заданным индексом");
+				}
+				catch (Exception ex)
+				{
+					logger.Fatal("Вызвана неизвестная ошибка при загрузки");
+					MessageBox.Show(ex.Message, "Неизвестная ошибка при загрузки", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				}
 			}
 		}
